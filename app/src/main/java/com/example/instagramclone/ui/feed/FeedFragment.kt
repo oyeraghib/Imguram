e25224d7326a63e57f81cd8463ac51ba5e79d0b4
oyeraghib <raghib.khan76@gmail.com>
2021-08-31 13:19:33 +0530

    Ui and Navigation updated adding hot and top fragment

---
+++ app/src/main/java/com/example/instagramclone/ui/feed/FeedFragment.kt
@@ -0,0 +1,17 @@
+package com.example.instagramclone.ui.feed
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
+import com.example.instagramclone.R
+import com.example.instagramclone.databinding.FragmentFeedBinding
+
+class FeedFragment: Fragment() {
+
+}
+
---
+++ app/src/main/java/com/example/instagramclone/ui/feed/FeedViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.instagramclone.ui.feed
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class FeedViewModel: ViewModel() {
+}
